c89df0c87be7cc032972442140c4c1e36c7d68cd
12philip34 <philipnssien@gmail.com>
2022-04-21 19:33:38 +0100

    updated form and html

--- public/assect/java/mysite.js
+++ public/assect/java/mysite.js
@@ -53,5 +53,22 @@ theme.onclick = () =>{
     theme.classList.add('fa-sun');
   }
 }
+function sendEmail (){
+      Email.send({
+        Host : "smtp.gmail.com",
+        Username : "nssienphilip@gmail.com",
+        Password : "kbohwzbtvekmnrbn",
+        To : 'nssienphilip@gmail.com',
+        From : document.getElementById('yourEmail').value,
+        Subject : "New Contact Form Enquiry",
+        Body : "Name:" + document.getElementById('yourName').value
+         + '<br> Email:' + document.getElementById('yourEmail').value
+         + '<br> Message:' + document.getElementById('textArea').value
+
+    }).then(
+      message => alert('Message Sent Successful!!')
+    );
+    
+}
 
 AOS.init();
